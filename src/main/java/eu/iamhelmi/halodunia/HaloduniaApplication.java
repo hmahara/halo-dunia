5950eb4a9f05189a0bd7fed90deecd6d026aee92
Helmi Mahara <helmi@lincsafe.com>
2023-12-20 16:42:00 +0100

    setup project

---
+++ src/main/java/eu/iamhelmi/halodunia/HaloduniaApplication.java
@@ -0,0 +1,13 @@
+package eu.iamhelmi.halodunia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloduniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaloduniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/iamhelmi/halodunia/HaloduniaApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.iamhelmi.halodunia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaloduniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
